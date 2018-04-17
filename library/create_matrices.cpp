59bfcd9c86940065dc27fc76725eb9df069909c7
Charles Cheng <charlescheng0117@gmail.com>
2018-04-17 22:13:11 +0800

    complete create_matrices.cpp

---
+++ library/create_matrices.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    // row: m, col: n
+    int m = 3, n = 5;
+
+    // with array, create
+    int ** oldM = new int*[n];
+    for (int i = 0; i < m; ++i) {
+        oldM[i] = new int[n];
+    }
+
+    // with array, delete
+    for (int i = 0; i < m; ++i) {
+        delete [] M[i];
+    }
+    delete [] M;
+
+    // use STL vectors
+    vector< vector<int> > M(m, vector<int>(n));
+
+    return 0;
+}
