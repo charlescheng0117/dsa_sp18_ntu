1e38b1a7539e1ed9eb0029def5f04cb3d92c5d22
Charles Cheng <charlescheng0117@gmail.com>
2018-05-16 20:34:32 +0800

    first commit of hw5

---
+++ hw/hw5/dp.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+typedef long long int lli;
+
+
+template <typename T>
+void print(vector<T> v) {
+    for (int i = 0; i < v.size(); ++i)
+        cout << v[i] << " ";
+    cout << "\n";
+}
+
+int main(int argc, char *argv[]) {
+    
+    int n; // days
+    fscanf(stdin, "%d", &n);
+
+    vector<lli> fries(n); // # of fries in each of n days;
+
+    for (int i = 0; i < n; ++i) {
+        fscanf(stdin, "%lli", &fries[i]);
+    } 
+    
+    printf("%d\n", n);
+    print(fries);
+
+
+    return 0;
+}
