7ad82f9b9ffbea1af5988a7a65506032a4b2867d
Charles Cheng <charlescheng0117@gmail.com>
2018-05-30 12:00:52 +0800

    try single_pig method

--- hw/hw5/bird.cpp
+++ hw/hw5/bird.cpp
@@ -4,6 +4,10 @@
 #include <cmath>
 #include <utility> // pair
 #include <map>
+#include <bitset> // count 1
+#include <unordered_set> // record single pig
+#include <climits>
+
 
 using namespace std;
 typedef long long int lli;
@@ -11,6 +15,7 @@ typedef long long int lli;
 #define epsilon 10e-7
 #define PIG 1
 #define NOPIG 0
+//#define DEBUG 1
 
 struct point {
     int x, y;
@@ -66,6 +71,10 @@ void show_pigs(int num_pigs, int pig_set) {
     printf("\n");
 }
 
+int compute_single(int pig_set_single) {
+    std::bitset<sizeof(int) * CHAR_BIT> b(pig_set_single);
+    return b.count();
+}
 
 coef compute_coef(point& p0, point& p1) {
     // compute the coefficient of: y = a x^2 + b x
@@ -81,12 +90,16 @@ coef compute_coef(point& p0, point& p1) {
     x1 = p1.x;
     y1 = p1.y;
 
-    if (x0 == x1)
+    if (x0 == x1) {
+        // should record two parabola for both points
         return coef(0, 0);
+    }
 
     double a = (x1 * y0 - x0 * y1) / (double) ( x0 * x1 * (x0 - x1) );
-    if (a >= 0)
-        return coef(0, 0);
+    if (a >= 0) {
+        // should record two parabola for both points
+        return coef(1, 0);
+    }
 
     double b = (y1 - a * x1 * x1) / (double) x1;
     
@@ -110,9 +123,12 @@ bool is_on_parabola(coef& cf, point& p) {
 
 //TODO iterate all pigs, compuate all coef, record all points on that parabola
 //vector<coef> compute_all_parabola(vector<point>& pigs) {
-void compute_all_parabola(vector<point>& pigs, map<coef, int>& map_pig) {
+void compute_all_parabola(vector<point>& pigs, map<coef, int>& map_pig, unordered_set<int>& set_non_single_pig, int& pig_set_single) {
+    // if we found multiple pigs on a parabola
+    // we substract it from 1111...1111 to form the pig_set_single
     int num_pigs = pigs.size();
-    map<coef, int>::iterator it;
+    map<coef, int>::iterator map_it;
+    unordered_set<int>::iterator set_it;
 
     for (int i = 0; i < num_pigs; ++i) {
         for (int j = 0; j < i; ++j) {
@@ -120,45 +136,84 @@ void compute_all_parabola(vector<point>& pigs, map<coef, int>& map_pig) {
             printf("current point: %d: [%d, %d], %d: [%d, %d]\n", i, pigs[i].x, pigs[i].y, j, pigs[j].x, pigs[j].y);
             #endif    
         
+            point pig_i = pigs[i], pig_j = pigs[j];
 
-            coef cf = compute_coef(pigs[i], pigs[j]);
-            it = map_pig.find(cf);
+            coef cf = compute_coef(pig_i, pig_j);
+            map_it = map_pig.find(cf);
 
-            if (it != map_pig.end()) {
-                int pig_set = it->second;
+            if (map_it != map_pig.end()) {
+                int pig_set = map_it->second;
                 #ifdef DEBUG
-                printf("found curve: y = %f x^2 + %f x, pigs are: ", it->first.a, it->first.b);
+                printf("found curve: y = %f x^2 + %f x, pigs are: ", map_it->first.a, map_it->first.b);
                 show_pigs(num_pigs, pig_set);
                 #endif
             }  else { 
+                int pig_set_i = (1 << (num_pigs - i - 1)); 
+                int pig_set_j = (1 << (num_pigs - j - 1));
 
                 if (cf.a < 0) {
-                    int pig_set = 0; // use int to record which pig is on the parabola
-
                     // first we should add pigs[i], pigs[j] to the pig_set, because they must
                     // be on that parabola
-                    pig_set += (1 << (num_pigs - i - 1)); 
-                    pig_set += (1 << (num_pigs - j - 1));
+                    // and we use int to record which pig is on the parabola
+                    int pig_set = 0 + pig_set_i + pig_set_j; 
 
+                    // we maintain a set to record non-single pig
+                    // and update the int pig_set_single in the same time
+                    if ( (set_it = set_non_single_pig.find(pig_set_i)) == set_non_single_pig.end() ) {
+                        set_non_single_pig.insert(pig_set_i);
+                        pig_set_single -= pig_set_i;
+                    }
+
+                    if ( (set_it = set_non_single_pig.find(pig_set_j)) == set_non_single_pig.end() ) {
+                        set_non_single_pig.insert(pig_set_j);
+                        pig_set_single -= pig_set_j;
+                    }
+
+                    // TODO add not single pigs into unordered_set
 
                     for (int k = 0; k < num_pigs; ++k) { // iterate all pigs to record 
                         if (k != i && k != j) {
                             if (is_on_parabola(cf, pigs[k])) {
-                                pig_set += (1 << (num_pigs - k - 1));
+                                int pig_set_k = (1 << (num_pigs - k - 1));
+
+                                if ( (set_it = set_non_single_pig.find(pig_set_k)) == set_non_single_pig.end() ) {
+                                    set_non_single_pig.insert(pig_set_k);
+                                    pig_set_single -= pig_set_k;
+                                }
+
+                                pig_set += pig_set_k;
                             }
                         }
                     
                     }
+
                     map_pig[cf] = pig_set;
                     #ifdef DEBUG
                     printf("y = %f x^2 + %f x has pigs: ", cf.a, cf.b);
                     show_pigs(num_pigs, pig_set);
                     #endif
                 } else {
+                //else if (cf.a == 0) {
+                    // case: x0 == x1, suppose we always assign a = -1
+                    // y = ax^2 + bx
+                    // b = (y + x^2) / x
+                    
+                    /* record single pig in map_pig is inefficient. so we comment out here. */
+                    //int xi = pig_i.x, yi = pig_i.y; 
+                    //int xj = pig_j.x, yj = pig_j.y;
+            
+                    //coef coef_i = coef(-1, (yi + xi * xi)/ (double)xi );
+                    //coef coef_j = coef(-1, (yj + xj * xj)/ (double)xj );
+
+                    //pig_set_single = pig_set_single + pig_set_i + pig_set_j;
+
+                    //map_pig[coef_i] = pig_set_i;
+                    //map_pig[coef_j] = pig_set_j;
+
+
                     #ifdef DEBUG
                     printf("current point: [%d, %d], [%d, %d] can't form a parabola with a < 0.\n", pigs[i].x, pigs[i].y, pigs[j].x, pigs[j].y);
                     #endif
-                    ;
                 }
              }
         }
@@ -182,7 +237,8 @@ int dp(vector<point>& pigs, int pig_set, map<coef, int>& map_pig) {
     int total = 0;
     int tmp;
     //for (auto& cf_pigset : map_pig) {
-    for (map<coef, int>::iterator it = map_pig.begin(); it != map_pig.end(); ++it) {
+    map<coef, int>::iterator map_pig_end = map_pig.end();
+    for (map<coef, int>::iterator it = map_pig.begin(); it != map_pig_end; ++it) {
         // pig_left is S XOR with the pigs on the parabola
         // TODO implement S / A
         // should also check if pig_set is on the map_pig
@@ -192,12 +248,14 @@ int dp(vector<point>& pigs, int pig_set, map<coef, int>& map_pig) {
         //     11011
         //     -----
         //     11011
-       
         
         int pig_left = pig_set & (~ it->second);
         #ifdef DEBUG
         printf("parabola is: ");
         it->first.print();
+
+        printf("pigs before: ");
+        show_pigs(pigs.size(), pig_set);
 
         printf("pigs remained: ");
         show_pigs(pigs.size(), pig_left);
@@ -241,6 +299,7 @@ int main(int argc, char *argv[]) {
         fscanf(stdin, "%d", &n);
 
         map<coef, int> map_pig;
+        unordered_set<int> set_non_single_pig;
         
         vector<point> pigs(n); // vector of the location of the pigs
         int x, y;
@@ -255,15 +314,31 @@ int main(int argc, char *argv[]) {
         // set pig_set = 111111111, n 1's
         // so we want 000000111111, n 1's
         int pig_set = (1 << n) - 1;
+        int pig_set_single = pig_set; // initialize as the same as pig_set
         #ifdef DEBUG
         print(pigs); 
         printf("showing initial %d pigs: \n", n);
         show_pigs(n, pig_set);
         #endif
-        compute_all_parabola(pigs, map_pig);
-        int total_shots = dp(pigs, pig_set, map_pig);
+        compute_all_parabola(pigs, map_pig, set_non_single_pig, pig_set_single);
+        
+        // compute the # of single pigs
+        int num_single = compute_single(pig_set_single);
+
+        // dp to solve the total shots needed for non-single pigs
+        int pig_set_not_single = pig_set & (~ pig_set_single);
+        #ifdef DEBUG
+        printf("num_single: %d\n", num_single);
+        printf("num_nonsingle: %d\n", compute_single(pig_set_not_single));
+        printf("show single pigs: ");
+        show_pigs(n, pig_set_single);
+        printf("show non_single pigs: ");
+        show_pigs(n, pig_set_not_single);
+        #endif
+
+
+        int total_shots = dp(pigs, pig_set_not_single, map_pig) + num_single;
         printf("%d\n", total_shots);
-        //printf("--------------------\n");
     }
     
 
